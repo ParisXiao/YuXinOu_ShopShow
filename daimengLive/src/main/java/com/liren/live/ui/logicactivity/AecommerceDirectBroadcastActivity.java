22cfc2e64df65ffbd8e98fae5d989e1088f9c331
JJC <958049096@qq.com>
2018-04-15 21:03:42 +0800

    界面提交

---
+++ daimengLive/src/main/java/com/liren/live/ui/logicactivity/AecommerceDirectBroadcastActivity.java
@@ -0,0 +1,38 @@
+package com.liren.live.ui.logicactivity;
+
+import android.view.View;
+
+import com.liren.live.R;
+import com.liren.live.base.BaseActivity;
+
+/**
+ * Created by JJC on 2018/4/15.
+ */
+
+public class AecommerceDirectBroadcastActivity extends BaseActivity{
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_aecommerce_direct_broadcast;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    protected boolean hasActionBar() {
+        return false;
+    }
+}
---
+++ daimengLive/src/main/java/com/liren/live/ui/logicactivity/ResetPassActivity.java
@@ -0,0 +1,37 @@
+package com.liren.live.ui.logicactivity;
+
+import android.view.View;
+
+import com.liren.live.R;
+import com.liren.live.base.BaseActivity;
+
+/**
+ * Created by JJC on 2018/4/15.
+ */
+
+public class ResetPassActivity extends BaseActivity {
+    @Override
+    protected boolean hasActionBar() {
+        return false;
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_reset_pass;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
